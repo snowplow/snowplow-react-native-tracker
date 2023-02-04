eff2f7763b7346d0fabf576faa480b7bf8f562f8
Matus Tomlein <matus.tomlein@gmail.com>
2023-02-04 09:20:30 +0100

    Add JavaScript tracker core to be used in Expo managed workflow when native trackers are not available (close #188)
    PR #184

--- src/native.ts
+++ src/native.ts
@@ -14,8 +14,20 @@
 'use strict';
 
 import { NativeModules } from 'react-native';
+import { JSSnowplowTracker } from './jsCore';
+import type { Native } from './types';
+import { errorHandler } from './utils';
 
-const { RNSnowplowTracker } = NativeModules;
+const isAvailable = NativeModules.RNSnowplowTracker != null;
+if (!isAvailable) {
+  errorHandler(
+    new Error(
+      'Unable to access the native iOS/Android Snowplow tracker, a tracker implementation with very limited functionality is used.'
+    )
+  );
+}
+
+const RNSnowplowTracker: Native = isAvailable ? NativeModules.RNSnowplowTracker as Native : JSSnowplowTracker;
 
 export {
   RNSnowplowTracker
