262fff86e8805ffe7ba795390e4a13f7398e2567
Matus Tomlein <matus.tomlein@gmail.com>
2023-09-19 11:16:32 +0200

    Disable automatic screen view tracking in the iOS and Android tracker by default (close #198)
    PR #202

--- src/types.ts
+++ src/types.ts
@@ -170,8 +170,10 @@ export interface TrackerConfiguration {
    */
   screenContext?: boolean;
   /**
-   * Whether enable automatic tracking of ScreenView events.
-   * @defaultValue true
+   * Whether to enable automatic tracking of ScreenView events from the native side.
+   * Only tracking UIKit views on iOS and Activity on Android are supported.
+   * For tracking React Native views, see the tracker docs for manual and auto-tracking options.
+   * @defaultValue false
    */
   screenViewAutotracking?: boolean;
   /**
