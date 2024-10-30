f2b7cff9b9e98ea6255c128a7a3d6c31364529b0
Matus Tomlein <matus.tomlein@gmail.com>
2024-10-30 16:56:23 +0100

    Fix crash on iOS on React Native new architecture (close #212)
    PR #213

--- ios/ReactNativeTracker.mm
+++ ios/ReactNativeTracker.mm
@@ -2,10 +2,6 @@
 
 @interface RCT_EXTERN_MODULE(ReactNativeTracker, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
-
 RCT_EXTERN_METHOD(createTracker:
                   (NSDictionary *)argmap
                   resolver:(RCTPromiseResolveBlock)resolve
